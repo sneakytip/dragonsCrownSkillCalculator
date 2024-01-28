e0c449079f9356ee95830f110da1e3fcd92350ed
Carlos Quinto <carlquin77@gmail.com>
2024-01-27 23:31:42 -0700

    update display, update main functions

--- main.js
+++ main.js
@@ -1,21 +1,49 @@
 const characterClassNames = ["Fighter","Amazon","Dwarf","Elf","Sorceress","Wizard"];
+const maxSkillPoints = 155;
+
+let playerSelectionPrevious = '';
+let currentSkillPoints = maxSkillPoints;
+
+///////////////////////////////////////////////
+// HTML skill tables are dynamically populated
+///////////////////////////////////////////////
+function populateSkillTable(source, target) {
+	let targetTable = document.getElementById(target);
+	let headerRow = document.createElement("tr");
+	let headerCell = document.createElement("th");
+	let headerText = document.createTextNode(source.label.toString());
+	
+	headerCell.appendChild(headerText);
+	headerRow.appendChild(headerCell);
+	targetTable.appendChild(headerRow);
+}
 
 function updateSkills() {
+	const tablesContainer = document.getElementById('tables-container');
 	const elemClassList = document.getElementById("characters-list");
 	let selectionCurr = elemClassList.value;
-	let skillsClassName = selectionCurr;
-	let skillsClass = getSkillType(skillsClassName);
-	let skillsCommon = getSkillType("common");
 	
-	console.log(skillsCommon);
-	console.info(skillsClass);
+	if (selectionCurr !== playerSelectionPrevious) {
+		let skillsClass = getSkillType(selectionCurr);
+		let skillsCommon = getSkillType("common");
+		
+		playerSelectionPrevious = selectionCurr;
+		currentSkillPoints = maxSkillPoints;
+		populateSkillTable(skillsCommon, "common-skills-table");
+		populateSkillTable(skillsClass, "class-skills-table");
+		
+		if (tablesContainer.style.display !== "block") {
+			tablesContainer.style.display = "block";
+		}
+		
+		console.log(skillsCommon);
+		console.info(skillsClass);
+	}
 }
 
-if (document.readyState === "loading") {
-  document.addEventListener("DOMContentLoaded", prepCollapse);
-} else {
-  prepCollapse();
-}
+////////////////////////////////////////////////
+// Instruction section expand/collapse features
+////////////////////////////////////////////////
 
 function prepCollapse() {
 	let collapse = document.getElementsByClassName("collapsible");
@@ -31,4 +59,10 @@ function prepCollapse() {
 		} 
 	  });
 	}
+}
+
+if (document.readyState === "loading") {
+  document.addEventListener("DOMContentLoaded", prepCollapse);
+} else {
+  prepCollapse();
 }
